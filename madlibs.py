13f8e9589085385f5800a6e44b6e0f105551e04a
Cole Callaway <nccallaway54@gmail.com>
2022-07-16 15:30:17 -0600

    done

--- madlibs.py
+++ madlibs.py
@@ -1,6 +1,7 @@
 """A madlib game that compliments its users."""
 
 from random import choice
+from turtle import color
 
 from flask import Flask, render_template, request
 
@@ -49,6 +50,19 @@ def show_madlib_form():
           return render_template('game.html')
       else:
           return render_template("goodbye.html") 
+@app.route('/madlib')    
+def show_madlib():
+    name = request.args.get('person')
+    color = request.args.get('color')
+    noun = request.args.get('noun')
+    adjective = request.args.get('adjective')
+    
+    return render_template('madlib.html', 
+                           person=name, 
+                           color=color, 
+                           noun=noun,
+                           adjective=adjective)
+    
 
 if __name__ == '__main__':
     # Setting debug=True gives us error messages in the browser and also
